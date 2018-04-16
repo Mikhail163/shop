ff0788c0e8d36d463674ed904616906cd4abf7fc
Mikhail163 <mephist0_87@mail.ru>
2018-04-16 23:00:08 +0400

    think over the structure of the site

--- gulpfile.js
+++ gulpfile.js
@@ -4,6 +4,7 @@ var sass = require('gulp-sass');
 var cssPrefix = require('gulp-autoprefixer'); // корректируем css
 var uglifyjs = require('gulp-uglify-es').default;
 var BS = require('browser-sync');
+var reload = BS.reload;
 
 
 // Определяем, какую сборку собираем
@@ -16,16 +17,26 @@ var config = {
 }
 
 
-gulp.task('default', ['html', 'sass', 'js', 'mywatch', 'server'], function () {
+gulp.task('default', ['html', 'sass', 'js', 'json', 'mywatch', 'server'], function () {
     console.log('task default');
 });
 
 gulp.task('html', function () {
     gulp.src([config.app + '/html/*.html'])
-        .pipe(gulp.dest(config.dist));
+        .pipe(gulp.dest(config.dist))
+        .pipe(reload({
+            stream: true
+        }));
+
+});
+
+gulp.task('json', function () {
 
     gulp.src([config.app + '/json/**/*.json'])
-        .pipe(gulp.dest(config.dist + '/json'));
+        .pipe(gulp.dest(config.dist + '/json'))
+        .pipe(reload({
+            stream: true
+        }));
 });
 
 gulp.task('sass', function () {
@@ -33,19 +44,26 @@ gulp.task('sass', function () {
     gulp.src([config.app + '/sass/main.scss'])
         .pipe(sass())
         .pipe(cssPrefix())
-        .pipe(gulp.dest(config.dist + '/css'));
+        .pipe(gulp.dest(config.dist + '/css'))
+        .pipe(reload({
+            stream: true
+        }));
 });
 
 gulp.task('js', function () {
     gulp.src([config.app + '/js/**/*.js'])
         .pipe(uglifyjs())
-        .pipe(gulp.dest(config.dist + '/js'));
+        .pipe(gulp.dest(config.dist + '/js'))
+        .pipe(reload({
+            stream: true
+        }));
 });
 
 gulp.task('mywatch', function () {
-    gulp.watch([config.app + '/html/lesson6.html'], ['html']);
+    gulp.watch([config.app + '/html/**/*.html'], ['html']);
     gulp.watch([config.app + '/sass/**/*.scss'], ['sass']);
     gulp.watch([config.app + '/js/**/*.js'], ['js']);
+    gulp.watch([config.app + '/json/**/*.json'], ['json']);
 });
 
 // Server
